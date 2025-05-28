7dfd9ebf77351baec51ccdaae22565dfa439167b
MrDoofer <thelionsmanejellyfish@gmail.com>
2025-05-28 16:43:07 -0400

    Geode fixed
    wasnt working before now is

--- Scripts/Geode.gd
+++ Scripts/Geode.gd
@@ -1,10 +1,14 @@
-extends Area2D
-@onready var animated_sprite_2d = $AnimatedSprite2D
-var geode = false
-func _on_body_entered():
-	geode =true
-func _on_body_exited():
-	geode = false
-func _process(delta):
-	if geode and Input.is_action_pressed("Interact"):
-		animated_sprite_2d.play("Cut")
+extends CollisionShape2D
+var Touchable = false
+@onready var animated_sprite_2d: AnimatedSprite2D = $"../AnimatedSprite2D"
+
+func _on_geode_body_entered(body: Node2D) -> void:
+	Touchable = true
+
+func _on_geode_body_exited(body: Node2D) -> void:
+	Touchable = false
+
+func _process(delta: float):
+	if Touchable==true and Input.is_action_just_pressed("Interact"):
+		animated_sprite_2d.play("Cracked")
+	

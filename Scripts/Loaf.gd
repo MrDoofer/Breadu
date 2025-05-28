2ef6a8b5396d9779fc7441f424f6b431ebcfbe88
MrDoofer <thelionsmanejellyfish@gmail.com>
2025-05-27 20:16:37 -0400

    EEEEEE

--- Scripts/Loaf.gd
+++ Scripts/Loaf.gd
@@ -1,12 +1,17 @@
 extends Area2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 var Loaf1 = false
+var interactable = true
 func _on_body_entered(body):
 	Loaf1 =true
+	interactable =true
 
 func _on_body_exited(body):
 	Loaf1 = false
+	interactable = false
 
 func _process(delta):
 	if Loaf1 and Input.is_action_pressed("Interact"):
 		animated_sprite_2d.play("Cut")
+	if animated_sprite_2d.is_playing("Cut"):
+		interactable =false

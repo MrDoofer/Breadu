45143eacdc1e1852ed57780105a10d024cafdf83
Mr Sigma <thelionsmanejellyfish@gmail.com>
2025-05-28 19:20:20 -0400

    got e to 160 regardless of where youre at

--- Scripts/geode.gd
+++ Scripts/geode.gd
@@ -2,12 +2,13 @@ extends Node2D
 var touched = false
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var area_2d: Area2D = $Area2D
-@onready var e: Sprite2D = $E
+@onready var e: Sprite2D = $CanvasLayer/E
+
 
 func _ready():
 	await get_tree().process_frame
 	e.hide()
-	e.global_position = Vector2(160,170)
+	e.position = Vector2(160, 160)
 	animated_sprite_2d.play("default")
 func _on_area_2d_body_entered(body: Node2D):
 	touched = true

d1bf3be3556ac6cf2d69a90c86d2e17bac0990fa
MrDoofer <thelionsmanejellyfish@gmail.com>
2025-05-30 18:49:41 -0400

    pick to respawn in front of you

--- Scripts/pickaxe.gd
+++ Scripts/pickaxe.gd
@@ -4,7 +4,16 @@ var pickedup =false
 @onready var item_popup: Sprite2D = $CanvasLayer/ItemPopup
 @onready var animatable_body_2d: AnimatableBody2D = $AnimatableBody2D
 @onready var burbur: CharacterBody2D = $"../../Burbur"
-
+func _show_all(node):
+	node.show()
+	for child in node.get_children():
+		if child is Node:
+			_show_all(child)
+func _hide_all(node):
+	node.hide()
+	for child in node.get_children():
+		if child is Node:
+			_hide_all(child)
 func _ready():
 	await get_tree().process_frame
 	animatable_body_2d.hide()
@@ -24,15 +33,15 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		animatable_body_2d.hide()
 	elif pickedup == false:
 		animatable_body_2d.show()
-
 func _process(delta: float):
-	if touchable and Input.is_action_just_pressed("Items"):
-		pickedup = true
-		_hide_all(self)
-		Global.pickaxeinhand = true
-
-func _hide_all(node):
-	node.hide()
-	for child in node.get_children():
-		if child is Node:
-			_hide_all(child)
+	if Input.is_action_just_pressed("Items"):
+		if touchable and not pickedup:
+			pickedup = true
+			_hide_all(self)
+			Global.pickaxeinhand = true
+		elif pickedup:
+			pickedup = false
+			self.position = burbur.position + Vector2(4, -8)
+			item_popup.position = Vector2(160, 160)
+			_show_all(self)
+			Global.pickaxeinhand = false

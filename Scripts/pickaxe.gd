b6630fae58fb0c4041ca7c8d8491e52df9772d6f
MrDoofer <thelionsmanejellyfish@gmail.com>
2025-05-31 14:40:18 -0400

    Finally pickaxe is solid for now

--- Scripts/pickaxe.gd
+++ Scripts/pickaxe.gd
@@ -1,8 +1,10 @@
 extends Node2D
 var geodes_available = false
+var body_entered = false
 var touchable = false
-@onready var item_popup: Sprite2D = $CanvasLayer/ItemPopup
+@onready var item_popup: Sprite2D = $"../../screen things/Camera2D/CanvasLayer/ItemPopup"
 @onready var burbur: CharacterBody2D = $"../../Burbur"
+
 func _hide_all(node):
 	if node is CanvasItem:
 		node.visible = false
@@ -34,23 +36,26 @@ func _ready():
 	item_popup.hide()
 	item_popup.position = Vector2(160,160)
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	body_entered = true
 	if body == burbur and Global.pickaxeinhand == false:
 		touchable = true
 		item_popup.show()
 func _on_area_2d_body_exited(body: Node2D) -> void:
+	body_entered = false
 	if body == burbur and Global.pickaxeinhand == false:
 		touchable = false
 		item_popup.hide()
 func _process(delta: float):
-	if not self.hidden:
-		geodes_available=Input.is_action_pressed("Geodes")
 	if Input.is_action_just_pressed("Items"):
 		if touchable and not Global.pickaxeinhand:
 			self.rotation = 0
 			Global.pickaxeinhand = true
+			touchable = false  # 💡 Important: Reset it
+			body_entered = false  # Also reset
 			_hide_all(self)
-		elif Global.pickaxeinhand:
+			item_popup.hide()
+		elif not touchable and Global.pickaxeinhand:
 			Global.pickaxeinhand = false
 			_show_all(self)
+			item_popup.hide()  # 👈 Force it to hide when re-spawning
 			self.position = burbur.position + Vector2(4, -20)
-			item_popup.position = Vector2(160, 160)

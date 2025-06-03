641e3be92d9a8cef6c013f0b602fa0ce2d424957
MrDoofer <thelionsmanejellyfish@gmail.com>
2025-06-03 12:35:24 -0400

    grass

--- Scripts/grass.gd
+++ Scripts/grass.gd
@@ -1,4 +1,11 @@
 extends Node2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	null
+
+
+func _on_static_body_2d_body_entered(body: Node2D) -> void:
+	animated_sprite_2d.play("stepped on")
+
+
+func _on_static_body_2d_body_exited(body: Node2D) -> void:
+	animated_sprite_2d.play("steepped off")

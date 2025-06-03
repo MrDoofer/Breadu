72d7e13226f344f3467256694b9ac5b17dd66391
MrDoofer <thelionsmanejellyfish@gmail.com>
2025-06-03 11:13:39 -0400

    burbur done

--- Scripts/grass.gd
+++ Scripts/grass.gd
@@ -1,15 +1,4 @@
-extends Area2D
+extends Node2D
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var animated_sprite = $"../G2/AnimatedSprite2D"
-
-
-func _on_body_entered(CharacterBody2D):
-	animated_sprite_2d.play("Stepped on")
-func _on_body_exited(CharacterBody2D):
-	animated_sprite_2d.play("Released")
-
-func _on_g_2_body_entered(CharacterBody2D):
-	animated_sprite.play("Stepped On")
-func _on_g_2_body_exited(CharacterBody2D):
-	animated_sprite.play("Released")
+func _physics_process(delta: float) -> void:
+	null
